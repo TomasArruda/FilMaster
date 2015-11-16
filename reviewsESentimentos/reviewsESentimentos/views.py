31cf4007d5a52618942d4eb2d376e9a1ab4799fc
robnff <robertsonfilho2@gmail.com>
2015-11-16 01:05:13 -0300

    commit com a estrutura django
    ainda falta editar algumas coisas no request, to upando aqui para voces
    ja terem onde ir juntando

---
+++ reviewsESentimentos/reviewsESentimentos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.shortcuts import render_to_response, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.template import RequestContext
+from reviewsESentimentos import settings
+
+# Create your views here.
+def home(request):
+	#essas linhas devem ser descomentadas quando blog for implementado e as categorias puxadas dele
+	#test = Test.objects.get(id=test_id)
+	#context = {}
+	return render_to_response(request,'home.html')
